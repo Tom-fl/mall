7a2a0ce1ed504f0648e51807728ef17eab4b18fb
DESKTOP-EVI1PV6\Tom <fengluo>
2021-09-13 23:30:57 +0800

    home组件的渲染

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-07-31 14:25:52
- * @LastEditTime: 2021-08-03 22:40:43
+ * @LastEditTime: 2021-09-05 17:06:38
  * @LastEditors: Tom
  * @Description: In User Settings Edit
  * @FilePath: \supermall\src\views\home\Home.vue
@@ -11,130 +11,33 @@
     <nav-bar class="home_nav">
       <div slot="center">购物街</div>
     </nav-bar>
-    <HomeSwiper :banners_son="banners" />
-    <HomeRecommendView :recommends_son="recommends" />
-    <HomeFeatureView />
-    <TabControl class="tab_control"
-                :titles="['流行', '新款', '精选']" />
-    <ul>
-      <li>列表1</li>
-      <li>列表2</li>
-      <li>列表3</li>
-      <li>列表4</li>
-      <li>列表5</li>
-      <li>列表6</li>
-      <li>列表7</li>
-      <li>列表8</li>
-      <li>列表9</li>
-      <li>列表10</li>
-      <li>列表11</li>
-      <li>列表12</li>
-      <li>列表13</li>
-      <li>列表14</li>
-      <li>列表15</li>
-      <li>列表16</li>
-      <li>列表17</li>
-      <li>列表18</li>
-      <li>列表19</li>
-      <li>列表20</li>
-      <li>列表21</li>
-      <li>列表22</li>
-      <li>列表23</li>
-      <li>列表24</li>
-      <li>列表25</li>
-      <li>列表26</li>
-      <li>列表27</li>
-      <li>列表28</li>
-      <li>列表29</li>
-      <li>列表30</li>
-      <li>列表31</li>
-      <li>列表32</li>
-      <li>列表33</li>
-      <li>列表34</li>
-      <li>列表35</li>
-      <li>列表36</li>
-      <li>列表37</li>
-      <li>列表38</li>
-      <li>列表39</li>
-      <li>列表40</li>
-      <li>列表41</li>
-      <li>列表42</li>
-      <li>列表43</li>
-      <li>列表44</li>
-      <li>列表45</li>
-      <li>列表46</li>
-      <li>列表47</li>
-      <li>列表48</li>
-      <li>列表49</li>
-      <li>列表50</li>
-      <li>列表51</li>
-      <li>列表52</li>
-      <li>列表53</li>
-      <li>列表54</li>
-      <li>列表55</li>
-      <li>列表56</li>
-      <li>列表57</li>
-      <li>列表58</li>
-      <li>列表59</li>
-      <li>列表60</li>
-      <li>列表61</li>
-      <li>列表62</li>
-      <li>列表63</li>
-      <li>列表64</li>
-      <li>列表65</li>
-      <li>列表66</li>
-      <li>列表67</li>
-      <li>列表68</li>
-      <li>列表69</li>
-      <li>列表70</li>
-      <li>列表71</li>
-      <li>列表72</li>
-      <li>列表73</li>
-      <li>列表74</li>
-      <li>列表75</li>
-      <li>列表76</li>
-      <li>列表77</li>
-      <li>列表78</li>
-      <li>列表79</li>
-      <li>列表80</li>
-      <li>列表81</li>
-      <li>列表82</li>
-      <li>列表83</li>
-      <li>列表84</li>
-      <li>列表85</li>
-      <li>列表86</li>
-      <li>列表87</li>
-      <li>列表88</li>
-      <li>列表89</li>
-      <li>列表90</li>
-      <li>列表91</li>
-      <li>列表92</li>
-      <li>列表93</li>
-      <li>列表94</li>
-      <li>列表95</li>
-      <li>列表96</li>
-      <li>列表97</li>
-      <li>列表98</li>
-      <li>列表99</li>
-      <li>列表100</li>
-    </ul>
+    <scroll class="content">
+      <HomeSwiper :banners_son="banners" />
+      <recommend-view :recommends_son="recommends" />
+      <feature-view />
+      <tab-control @tabClick="tabClick" class="tab_control" ref="tabControl1" :titles="['流行', '新款', '精选']" />
+      <goods-list :goods="showGoods" />
+    </scroll>
   </div>
 </template>
 
 <script>
-import HomeSwiper from "./childComps/HomeSwiper";
-import HomeRecommendView from "./childComps/HomeRecommendView";
-import HomeFeatureView from "./childComps/HomeFeatureView";
+import HomeSwiper from './childComps/HomeSwiper';
+import RecommendView from './childComps/RecommendView';
+import FeatureView from './childComps/FeatureView';
 
-import NavBar from "@/components/common/navbar/NavBar";
-import TabControl from "@/components/content/tabControl/TabControl.vue";
+import NavBar from '@/components/common/navbar/NavBar';
+import TabControl from '@/components/content/tabControl/TabControl.vue';
+import Scroll from '@/components/common/scroll/Scroll.vue';
+import GoodsList from '@/components/content/goods/GoodsList';
 
-import { getHomeMultidata, getHomeGoods } from "@/network/home.js";
+import { getHomeMultidata, getHomeGoods } from '@/network/home.js';
 
 export default {
-  name: "Home",
+  name: 'Home',
   data() {
     return {
+      currentIndex: '',
       banners: [],
       recommends: [],
       goods: {
@@ -142,36 +45,63 @@ export default {
         new: { page: 0, list: [] },
         sell: { page: 0, list: [] },
       },
+      currentType: 'pop',
     };
+  },
+  computed: {
+    showGoods() {
+      return this.goods[this.currentType].list;
+    },
   },
   components: {
     HomeSwiper,
-    HomeRecommendView,
-    HomeFeatureView,
+    RecommendView,
+    FeatureView,
+    GoodsList,
 
     NavBar,
     TabControl,
+    Scroll,
   },
   created() {
     // 1.请求多个数据
     this.getHomeMultidata();
     // 2.请求商品数据
-    this.getHomeGoods("pop");
-    this.getHomeGoods("new");
-    this.getHomeGoods("sell");
+    this.getHomeGoods('pop');
+    this.getHomeGoods('new');
+    this.getHomeGoods('sell');
   },
   methods: {
+    /**
+     * 事件监听相关的方法
+     */
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.currentType = 'pop';
+          break;
+        case 1:
+          this.currentType = 'new';
+          break;
+        case 2:
+          this.currentType = 'sell';
+          break;
+      }
+      this.$refs.tabControl1.currentIndex = index;
+      // this.$refs.tabControl2.currentIndex = index;
+    },
+    /**
+     * 网络请求相关的方法
+     */
     getHomeMultidata() {
-      getHomeMultidata().then((res) => {
+      getHomeMultidata().then(res => {
         this.banners = res.data.banner.list;
         this.recommends = res.data.recommend.list;
       });
     },
     getHomeGoods(type) {
       const page = this.goods[type].page + 1;
-      console.log(this.goods[type].page);
-      console.log(page);
-      getHomeGoods(type, page).then((res) => {
+      getHomeGoods(type, page).then(res => {
         this.goods[type].list.push(...res.data.list);
         this.goods[type].page += 1;
       });
@@ -183,6 +113,8 @@ export default {
 <style scoped lang="less">
 #home {
   padding-top: 44px;
+  height: 100vh;
+  position: relative;
   .home_nav {
     background-color: var(--color-tint);
     color: #fff;
@@ -197,5 +129,18 @@ export default {
     position: sticky;
     top: 43px;
   }
+  .content {
+    overflow: hidden;
+    position: absolute;
+    top: 44px;
+    bottom: 39px;
+    left: 0;
+    right: 0;
+  }
 }
+// .content {
+//   height: calc(100% - 93px);
+//   overflow: hidden;
+//   margin-top: 44px;
+// }
 </style>
